bc91ce069092d6fb0c4f9ac8ad95105da6b8d516
BFEMCC <114940465+BFEMCC@users.noreply.github.com>
2023-05-10 21:22:45 +0800

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,14 +1,26 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ …˙≥…µƒ∞¸∫¨Œƒº˛°£
-// π© LoginGUI.rc  π”√
+// Microsoft Visual C++ ÁîüÊàêÁöÑÂåÖÂê´Êñá‰ª∂„ÄÇ
+// ‰æõ LoginGUI.rc ‰ΩøÁî®
 //
 #define IDI_ICON1                       101
+#define IDB_PNG1                        102
+#define IDB_PNG2                        103
+#define IDB_PNG3                        104
+#define IDB_PNG4                        105
+#define IDB_PNG5                        106
+#define IDB_PNG6                        107
+#define IDB_PNG7                        108
+#define IDB_PNG8                        109
+#define IDB_PNG9                        110
+#define IDB_PNG10                       111
+#define IDB_PNG11                       112
+#define IDB_PNG12                       113
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        114
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
